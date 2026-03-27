16c9232c79cfda4b28d5a5a109ea137b65ea879c
naiyao peng <pengnaiyao@gmail.com>
2026-03-27 12:48:58 +0800

    Create CourseBuddyApplication.java

---
+++ src/main/java/com/coursebuddy/CourseBuddyApplication.java
@@ -0,0 +1,12 @@
+package com.coursebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseBuddyApplication.class, args);
+    }
+}
